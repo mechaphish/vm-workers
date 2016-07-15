8c393982d289f7f340353298050873acd40c1231
Aravind, Machiry <machiry@cs.ucsb.edu>
2016-07-15 14:03:06 -0700

    Making VM Worker Deployment Ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+      name='vm-worker',
+      version='0.01',
+      packages=['test_vm_worker'],
+      scripts=['bin/common_tester'],
+      description='Repo that contains components that need to run inside CGC VM.',
+      url='https://git.seclab.cs.ucsb.edu:cgc/vm-workers',
+)
