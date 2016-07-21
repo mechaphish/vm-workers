f6c845286f3867e9ed285655263b6162e19f4d1b
Aravind, Machiry <machiry@cs.ucsb.edu>
2016-07-21 00:15:12 -0700

    Adding job type as paramter

--- test_vm_worker/__init__.py
+++ test_vm_worker/__init__.py
@@ -8,10 +8,10 @@ from multiprocessing import Pool, cpu_count
 import time
 import sys
 
-worker_config = [('povtester', CRSAPIWrapper.get_all_povtester_jobs, process_povtester_job),
+worker_config = [('pov_tester', CRSAPIWrapper.get_all_povtester_jobs, process_povtester_job),
                  ('cb_tester', CRSAPIWrapper.get_all_cb_tester_jobs, process_cb_tester_job),
                  ('poll_creator', CRSAPIWrapper.get_all_poller_jobs, process_poll_creator_job),
-                 ('poll_sanitizer', CRSAPIWrapper.get_all_poll_sanitizer_jobs, process_sanitizer_job)]
+                 ('network_poll_sanitizer', CRSAPIWrapper.get_all_poll_sanitizer_jobs, process_sanitizer_job)]
 
 NO_OF_PROCESSES = cpu_count()
 POLL_TIME = 1  # Time to sleep, if no jobs are available to run
@@ -24,6 +24,8 @@ def run_daemon(arg_list):
     global NO_OF_PROCESSES
     global POLL_TIME
     target_cs_id = None
+    target_job_type = None
+    max_num_jobs = 1000
 
     if len(arg_list) > 1:
         try:
@@ -33,8 +35,19 @@ def run_daemon(arg_list):
             log_failure("Unable to parse the provided argument into CS ID:" + str(arg_list[1]))
             # Ignore the error
             pass
-
     if len(arg_list) > 2:
+        target_job_type = arg_list[2]
+
+    if len(arg_list) > 3:
+        try:
+            max_num_jobs = int(arg_list[3])
+        except ValueError:
+            log_failure("Unable to parse the provided argument into number of jobs:" + str(arg_list[3]))
+            # default, number of jobs
+            max_num_jobs = 1000
+
+    # Ignore this
+    """if len(arg_list) > 2:
         try:
             no_of_process = int(arg_list[2])
         except ValueError:
@@ -45,37 +58,41 @@ def run_daemon(arg_list):
             poll_time = int(arg_list[3])
         except ValueError:
             poll_time = POLL_TIME
-        POLL_TIME = poll_time
+        POLL_TIME = poll_time"""
+
     if EXIT_ON_WRONG_CS_ID and target_cs_id is None:
         log_error("Exiting, without scheduling any jobs as no valid CS ID is provided.")
         return
     elif target_cs_id is None:
         log_info("Will be running infinitely fetching Jobs for all CS.")
+    processed_jobs = 0
     while True:
         CRSAPIWrapper.open_connection()
         no_jobs = True
         for curr_worker_config in worker_config:
             worker_name = curr_worker_config[0]
-            job_getter = curr_worker_config[1]
-            job_processor = curr_worker_config[2]
-            log_info("Trying to get " + worker_name + " Jobs.")
-            available_jobs = job_getter(target_cs_id=target_cs_id)
-            if len(available_jobs) > 0:
-                log_info("Got " + str(len(available_jobs)) + " " + worker_name + " Jobs.")
-                child_threads = NO_OF_PROCESSES / len(available_jobs)
-                child_job_args = map(lambda curr_job: (curr_job, child_threads), available_jobs)
-                process_pool = Pool(processes=NO_OF_PROCESSES)
-                process_pool.map(job_processor, child_job_args)
-                process_pool.close()
-                process_pool.join()
-                log_success("Processed " + str(len(available_jobs)) + " " + worker_name + " Jobs.")
-                no_jobs = False
-                # start again from beginning.
-                # This will ensure the if there are any higher priority jobs, we always execute them
-                # before going to lower priority ones
-                break
-            else:
-                log_info("No " + worker_name + " Jobs available to run.")
+            if target_job_type is None or worker_name == target_job_type:
+                job_getter = curr_worker_config[1]
+                job_processor = curr_worker_config[2]
+                log_info("Trying to get " + worker_name + " Jobs.")
+                available_jobs = job_getter(target_cs_id=target_cs_id)
+                processed_jobs += len(available_jobs)
+                if len(available_jobs) > 0:
+                    log_info("Got " + str(len(available_jobs)) + " " + worker_name + " Jobs.")
+                    child_threads = NO_OF_PROCESSES / len(available_jobs)
+                    child_job_args = map(lambda curr_job: (curr_job, child_threads), available_jobs)
+                    process_pool = Pool(processes=NO_OF_PROCESSES)
+                    process_pool.map(job_processor, child_job_args)
+                    process_pool.close()
+                    process_pool.join()
+                    log_success("Processed " + str(len(available_jobs)) + " " + worker_name + " Jobs.")
+                    no_jobs = False
+                    # start again from beginning.
+                    # This will ensure the if there are any higher priority jobs, we always execute them
+                    # before going to lower priority ones
+                    break
+                else:
+                    log_info("No " + worker_name + " Jobs available to run.")
 
         # if there are no VM jobs
         if no_jobs:
@@ -84,8 +101,11 @@ def run_daemon(arg_list):
             # so that the worker knows this VM is done.
             if target_cs_id is not None:
                 break
-            time.sleep(poll_time)
+            time.sleep(POLL_TIME)
+        # if we processed sufficient number of jobs? then exit
+        if processed_jobs >= max_num_jobs:
+            break
 
 if __name__ == "__main__":
-    # Command line arguments: workers.py <target_cs_id> <NO_OF_PROCESSES> <POLL_TIME_IN_SECONDS>
+    # Command line arguments: common_tester <target_cs_id> <job_type_string> <max_num_jobs>
     run_daemon(sys.argv)
